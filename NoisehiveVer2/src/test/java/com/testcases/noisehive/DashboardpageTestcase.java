d674ace175a769913808f05b286ae955eef7f5a8
skau0002 <skau0002@synchronoss.com>
2019-10-17 16:49:17 +0530

    New method added

--- NoisehiveVer2/src/test/java/com/testcases/noisehive/DashboardpageTestcase.java
+++ NoisehiveVer2/src/test/java/com/testcases/noisehive/DashboardpageTestcase.java
@@ -1,14 +1,17 @@
 package com.testcases.noisehive;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import com.base.noisehive.Base;
 import com.pages.noisehive.Dashboardpage;
 
-public class DashboardpageTestcase extends Base {
+public class DashboardpageTestcase extends Base {	
 	
 	
 	@BeforeMethod
@@ -18,15 +21,26 @@ public class DashboardpageTestcase extends Base {
 		initBrowser();
 	}
 	
+	Dashboardpage dp = PageFactory.initElements(driver, Dashboardpage.class);
 	
-	@Test
+	@Test(priority=1)
 	public void testdashboardpage()
-	{
+	{	
 		
-		Dashboardpage dp = PageFactory.initElements(driver, Dashboardpage.class);
 		dp.verifyDashboardTitle();
 		
 	}
+	
+	
+	
+	  @Test(priority=2) 
+	  public void verifyDashboardMusicHeader()
+	  {
+		  
+	  dp.verifyDashboardHeader();
+	  
+	  }
+	 
 	
 	
 	@AfterMethod
